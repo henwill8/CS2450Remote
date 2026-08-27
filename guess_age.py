e55c0f5a43851c406cfd798f69f8b6af0f5c7f0c
Henwill8 <henwill8@yahoo.com>
2026-08-27 13:10:25 -0600

    limit guesses to 5

--- guess_age.py
+++ guess_age.py
@@ -2,10 +2,12 @@ import random
 
 print("Hello, I will try to guess your age.")
 name = input("What is your name? ")
-while True:
+for i in range(1, 6):
     guess = random.randint(15, 40)
-    if input(f"Are you {guess} years old? (y/n) ").lower() = "y":
+    if input(f"[Guess {i}/5] Are you {guess} years old? (y/n) ").lower() = "y":
         print(f"{name} is {guess} years old.")
         break
     print("Rats.")
     # cool comment
+
+print("I could not guess it")
