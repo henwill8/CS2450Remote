08778fedbbadd8962fd6cdae038e80e22c698bcd
Henwill8 <henwill8@yahoo.com>
2026-08-27 13:12:25 -0600

    prevent reguessing the same age

--- guess_age.py
+++ guess_age.py
@@ -2,11 +2,15 @@ import random
 
 print("Hello, I will try to guess your age.")
 name = input("What is your name? ")
+guessed_ages = set()
 for i in range(1, 6):
     guess = random.randint(15, 40)
+    while guess in guessed_ages:
+        guess = random.randint(15, 40)
     if input(f"[Guess {i}/5] Are you {guess} years old? (y/n) ").lower() = "y":
         print(f"{name} is {guess} years old.")
         break
+    guessed_ages.add(guess)
     print("Rats.")
     # cool comment
 
